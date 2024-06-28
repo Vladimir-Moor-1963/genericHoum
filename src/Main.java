033757038db6e6e5e918cfcfe7c6f1c1f73c4964
Vladimir <papa1963@hotmail.de>
2024-06-28 06:34:32 +0200

    comit

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Что вы хотите записать?");
+        System.out.println("1 строка");
+        System.out.println("2 число");
 
-        String string = scanner.nextLine();
-        Message<String> stringMessage = new Message<>(scanner.nextLine());
+        int result = scanner.nextInt();
+        scanner.nextLine();
+        switch (result){
+            case 1:
+                System.out.println("строка");
+                String string = scanner.nextLine();
+                Message<String> stringMessage = new Message<>(string);
+                System.out.println("успешно " + stringMessage);
+                break;
+            case 2:
+                System.out.println("число");
+                int number = scanner.nextInt();
+                Message<Integer> integerMessage = new Message<>(number);
+                System.out.println("успешно " + integerMessage);
+                break;
 
-        Integer integer = scanner.nextLine();
-        Message<Integer> integerMessage = new Message<>(scanner.nextLine());
 
-
-
-        if (scanner.nextLine() == string)
-            System.out.println("пишите " + stringMessage);
-        else {
-            System.out.println("Дайте число " + integerMessage );
         }
 
 
@@ -29,4 +36,12 @@ public class Main {
 
 
     }
-}
+
+
+
+
+
+
+
+    }
+
